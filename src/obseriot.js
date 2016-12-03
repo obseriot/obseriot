1dc0e80e4a09a87dad3243975351863d41f77da7
aggre <hiroyuki.aggre@gmail.com>
2016-12-03 16:40:46 +0900

    Revert "Improve safety"
    This reverts commit 8b51d17b39cdee1144a12ba72722ae3a8296613f.

--- src/obseriot.js
+++ src/obseriot.js
@@ -1,56 +1,33 @@
 import observable from 'riot-observable'
 
-var o = new function () {
-        observable( this )
-    },
-    obseriot = {}
+var obseriot = new function () {
+    observable( this )
+}
 
-Object.defineProperties( obseriot, {
-    observable: {
-        value: o
-    },
-    listen: {
-        value: function ( e, cb ) {
-            if ( ! e.handler ) return
-            o.on( e.handler.name, cb )
-        },
-        enumerable: false,
-        writable: false,
-        configurable: false
-    },
-    notify: {
-        value: function ( e, ...arg ) {
-            if ( ! e.handler ) return
-            let t = [ e.handler.name ],
-                f = e.handler.action.apply( this, arg )
-            if ( f.constructor.name !== 'Array' ) f = [ f ]
-            Array.prototype.push.apply( t, f )
-            o.trigger.apply( this, t )
-        },
-        enumerable: false,
-        writable: false,
-        configurable: false
-    },
-    once: {
-        value: function ( e, cb ) {
-            if ( ! e.handler ) return
-            o.one( e.handler.name, cb )
-        },
-        enumerable: false,
-        writable: false,
-        configurable: false
-    },
-    remove: {
-        value: function ( e, cb ) {
-            if ( ! e.handler ) return
-            let t = [ e.handler.name ]
-            if ( cb ) t.push( cb )
-            o.off.apply( this, t )
-        },
-        enumerable: false,
-        writable: false,
-        configurable: false
-    }
-} )
+obseriot.listen = function ( e, cb ) {
+    if ( ! e.handler ) return
+    this.on( e.handler.name, cb )
+}
+
+obseriot.notify = function ( e, ...arg ) {
+    if ( ! e.handler ) return
+    let t = [ e.handler.name ],
+        f = e.handler.action.apply( this, arg )
+    if ( f.constructor.name !== 'Array' ) f = [ f ]
+    Array.prototype.push.apply( t, f )
+    this.trigger.apply( this, t )
+}
+
+obseriot.once = function ( e, cb ) {
+    if ( ! e.handler ) return
+    this.one( e.handler.name, cb )
+}
+
+obseriot.remove = function ( e, cb ) {
+    if ( ! e.handler ) return
+    let t = [ e.handler.name ]
+    if ( cb ) t.push( cb )
+    this.off.apply( this, t )
+}
 
 export default obseriot
